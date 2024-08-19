f64b4a66fd257ae15baf5cade04a9129080f2711
Matheus de Gondra <matheusgondra951@gmail.com>
2024-08-19 12:46:33 -0300

    refactor: move sut creation to a factory method

--- test/application/controllers/signup-controller.spec.js
+++ test/application/controllers/signup-controller.spec.js
@@ -2,28 +2,38 @@ import assert from "node:assert";
 import { describe, it, mock } from "node:test";
 import { SignupController } from "../../../src/application/controllers/index.js";
 
+const makeValidationStub = () => {
+	class ValidationStub {
+		validate(input) {
+			return null;
+		}
+	};
+	return new ValidationStub();
+}
+
+const makeSut = () => {
+	const validatorStub = makeValidationStub();
+	const sut = new SignupController({ validation: validatorStub });
+	return { sut, validatorStub };
+};
+
+const makeFakeRequest = () => ({
+	body: {
+		first_name: "any_first_name",
+		last_name: "any_last_name",
+		email: "any_email@mail.com",
+		password: "any_password"
+	}
+});
+
 describe("SignupController", () => {
 	it("Should call Validation with correct value", async () => {
-		class ValidationStub {
-			validate(input) {
-				return null;
-			}
-		};
-		const validatorStub = new ValidationStub();
-		const sut = new SignupController({ validation: validatorStub });
+		const { sut, validatorStub } = makeSut();
 		mock.method(validatorStub, "validate");
-		const httpRequest = {
-			body: {
-				first_name: "any_first_name",
-				last_name: "any_last_name",
-				email: "any_email@mail.com",
-				password: "any_password"
-			}
-		};
-		sut.handle(httpRequest);
 
+		sut.handle(makeFakeRequest());
 		const calls = validatorStub.validate.mock.calls[0];
 
-		assert.deepEqual(calls.arguments, [httpRequest.body]);
+		assert.deepEqual(calls.arguments, [makeFakeRequest().body]);
 	});
 });
