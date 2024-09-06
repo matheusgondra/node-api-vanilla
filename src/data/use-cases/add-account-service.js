e922a96e36a2e501be18c8818e6a42e6cbfb4ecd
Matheus de Gondra <matheusgondra951@gmail.com>
2024-09-06 14:03:21 -0300

    feat: ensure AddAccountService call Encrypter with correct password

--- src/data/use-cases/add-account-service.js
+++ src/data/use-cases/add-account-service.js
@@ -1,11 +1,14 @@
 export class AddAccountService {
 	#addAccountRepository;
+	#encrypter;
 
-	constructor({ addAccountRepository }) {
+	constructor({ addAccountRepository, encrypter }) {
 		this.#addAccountRepository = addAccountRepository;
+		this.#encrypter = encrypter;
 	}
 
 	async add(account) {
+		await this.#encrypter.encrypt(account.password);
 		await this.#addAccountRepository.add(account);
 	}
 }
