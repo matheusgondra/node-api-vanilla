af7dddaf095f7c553d73d9e690d240231d14a334
matheusgondra <matheusgondra951@gmail.com>
2025-02-11 07:54:30 -0300

    test: ensure AccountRepository return an account on success

---
+++ test/infra/db/postgresql/account-repository.spec.js
@@ -0,0 +1,46 @@
+import { describe, it, beforeEach } from "node:test";
+import { deepStrictEqual, ok, strictEqual } from "node:assert";
+import pg from "pg";
+import { AccountRepository } from "../../../../src/infra/db/postgresql/account-repository.js";
+
+const makeSut = () => {
+	return new AccountRepository();
+}
+
+describe("AccountRepository", () => {
+	describe("add", () => {
+		beforeEach(async () => {
+			const client = new pg.Client();
+			await client.connect();
+			await client.query("DELETE FROM users");
+		});
+
+		it("Should return an account on success", async () => {
+			const sut = makeSut();
+			const account = await sut.add({
+				first_name: "any_first_name",
+				last_name: "any_last_name",
+				email: "any_email@mail.com",
+				password: "any_password"
+			});
+
+			deepStrictEqual({
+				first_name: account.first_name,
+				last_name: account.last_name,
+				email: account.email
+			},
+				{
+					first_name: "any_first_name",
+					last_name: "any_last_name",
+					email: "any_email@mail.com"
+				});
+
+			ok(account.id);
+			ok(account.created_at);
+			ok(account.updated_at);
+			strictEqual(typeof account.id, "number");
+			strictEqual(typeof account.created_at, typeof new Date());
+			strictEqual(typeof account.updated_at, typeof new Date());
+		});
+	});
+});
